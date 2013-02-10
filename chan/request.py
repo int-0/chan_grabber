43020a3602535061b689737fc6fed3379209d7cd
int-0 <tobias.deb@gmail.com>
2013-02-10 13:55:34 +0100

    Added thumbnail support

--- chan/request.py
+++ chan/request.py
@@ -2,6 +2,7 @@
 
 import json
 import urllib2
+import socket
 
 class InvalidRequest(Exception):
     def __str__(self):
@@ -11,13 +12,13 @@ def to_api(request):
     try:
         remote = urllib2.urlopen(request)
         return json.loads(remote.read())
-    except urllib2.HTTPError:
+    except (urllib2.HTTPError, socket.error):
         raise InvalidRequest()
 
 def to_file(request):
     try:
         remote = urllib2.urlopen(request)
         return remote.read()
-    except urllib2.HTTPError:
+    except (urllib2.HTTPError, socket.error):
         raise InvalidRequest()
 
--- chan/translator.py
+++ chan/translator.py
@@ -37,12 +37,16 @@ class Translator(object):
     def file(self, file_id):
         raise NotImplementedError()
 
+    def thumb(self, file_id):
+        raise NotImplementedError()
+
 # Translator for 4CHAN
 class T4Chan(Translator):
     def __init__(self, board_id):
         Translator.__init__(self, board_id)
         self._API_URL = 'http://api.4chan.org/'
         self._FILE_BASE = 'http://images.4chan.org/'
+        self._THUMB_BASE = 'http://thumbs,4chan.org/'
 
     def thread(self, thread_id):
         if thread_id < self.max_pages:
@@ -59,3 +63,8 @@ class T4Chan(Translator):
         return '%s%s/src/%s' % (self._FILE_BASE,
                                 self.board_id,
                                 file_id)
+
+    def thumb(self, file_id):
+        return '%s%s/thumb/%ss.jpg' % (self._THUMB_BASE,
+                                       self.board_id,
+                                       file_id)
